8473bdaad1bc8f2463e43d109a5492f906fda11d
Aayush <aayush23467@gmail.com>
2023-07-28 16:52:29 +0000

    login-api-2

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ mod web;
 
 use axum::{
     routing::get,
-    Router,
+    Router, response::Response, middleware,
 };
 use helper::routes::{routes_hello, routes_static};
 use self::error::{Error, Result};
@@ -15,10 +15,18 @@ async fn main() {
     let app = Router::new().route("/", get(|| async { "Hello, World!" }))
         .merge(routes_hello())
         .merge(web::routes_login::routes())
+        .layer(middleware::map_response(main_response_mapper))
         .fallback_service(routes_static());
     // run it with hyper on localhost:3000
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(app.into_make_service())
         .await
         .unwrap();
+}
+
+async fn main_response_mapper(res: Response) -> Response {
+    println!("->> {:<12} - main_response_mapper", "RES_MAPPER");
+
+    println!();
+    res
 }
